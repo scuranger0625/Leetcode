61c4e84cf6809703f367d408fa5b6996b3a1de44
Leon <166517099+scuranger0625@users.noreply.github.com>
2024-07-26 13:10:53 +0800

    Add files via upload

---
+++ createDataframe.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from typing import List
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    df = pd.DataFrame(student_data, columns=['student_id', 'age'])
+    return df
