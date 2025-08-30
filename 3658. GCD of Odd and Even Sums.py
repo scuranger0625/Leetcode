96a852608005f0ba237ab4f70b26e4a87dfbaa0a
Leon <166517099+scuranger0625@users.noreply.github.com>
2025-08-30 20:38:46 +0800

    Create 3658. GCD of Odd and Even Sums.py

---
+++ 3658. GCD of Odd and Even Sums.py
@@ -0,0 +1,8 @@
+from typing import List
+
+# 離散數學 關係式 (Relation)
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        # sumOdd = n^2, sumEven = n(n+1)
+        # gcd(sumOdd, sumEven) = n
+        return n
