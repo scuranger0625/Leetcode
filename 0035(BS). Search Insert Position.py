0892a84b53a56a84bbe4d2199ddaa3c7640880f4
Leon <166517099+scuranger0625@users.noreply.github.com>
2025-12-28 19:56:11 +0800

    Create 0035(BS). Search Insert Position.py

---
+++ 0035(BS). Search Insert Position.py
@@ -0,0 +1,16 @@
+# Leetcode, Ligmaball
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0 ,len(nums)
+
+        while left < right:
+            mid = (left+right)//2
+
+            if nums[mid]>= target:
+                right = mid
+            else:
+                left = mid+1
+        
+        return left
+
+        
